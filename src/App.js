467cb38800568b1612bfa9894e868e018d46cb0c
aaannestad <a.aannestad@gmail.com>
2022-04-21 15:28:39 -0500

    Some actual content and some better formatting!

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { TextElement, LinkElement } from "./pageelement";
 import Header from "./header";
 import { React, useState } from "react";
 
-//const currentLang = 'jp';
-
 export default function App() {
   const [lang, updateLang] = useState("en");
 
@@ -22,15 +20,51 @@ export default function App() {
   return (
     <div>
       <Header lang={lang} handleLangButton={handleLangButton} />
-      <TextElement lang={lang} texten="Hello World" textjp="こんにちは" />
-      <LinkElement
-        lang={lang}
-        link="c:\"
-        linktexten="Link"
-        desctexten="desctext"
-        linktextjp="リンク"
-        desctextjp="説明"
-      />
+      <div class="row">
+        <div class="col-1"></div>
+        <div class="col-2">
+          <TextElement lang={lang} texten="Hello World" textjp="こんにちは" />
+          <LinkElement
+            lang={lang}
+            link="c:\"
+            linktexten="Link"
+            desctexten="desctext"
+            linktextjp="リンク"
+            desctextjp="説明"
+          />
+          <TextElement
+            lang={lang}
+            texten={[
+              "And also this webpage, which you can view the source for ",
+              <a href="https://github.com/aaannestad/aaannestad.github.io">
+                here
+              </a>,
+              ". (Tap the button at the top right to switch languages.)",
+            ]}
+            textjp={[
+              "そしてこのページ自身。ソースは",
+              <a href="https://github.com/aaannestad/aaannestad.github.io">
+                こちらで
+              </a>,
+              "見ることができます。",
+            ]}
+          />
+          <hr></hr>
+          <TextElement
+            lang={lang}
+            texten="I've also done a number of other things, largely related to linguistics."
+            textjp="他にも言語学に関連したことはやったことがあります。"
+          />
+          <LinkElement
+            lang={lang}
+            link="c:\"
+            linktexten="TeX for linguists"
+            desctexten="an introduction to using TeX for linguistics work"
+            linktextjp="言語学者向けのTeX入門"
+            desctextjp="言語学論文やそのようなものをTeXで書く方法を紹介する文章（英語）"
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0"
+    ></meta>
     <App />
   </React.StrictMode>
 );
